93373c5400cd3dbacf91efd1292e861f35761e2a
Gulati <gulatimu@hyrdmacpro0047.es.ad.adp.com>
2019-01-08 19:45:42 +0530

    angular app with lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingmodule, routingComponents } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingmodule
   ],
   providers: [],
   bootstrap: [AppComponent]
